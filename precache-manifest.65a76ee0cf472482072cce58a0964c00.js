3ecbc7d8910ffd583cfcdbfdc97c15f607425479
K Vishnu <vishnu.a.k@capgemini.com>
2020-07-27 20:28:19 +0530

    Updates

---
+++ precache-manifest.65a76ee0cf472482072cce58a0964c00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ad087b8d719c8e63da879a25a5b19c6",
+    "url": "/menu-builder/index.html"
+  },
+  {
+    "revision": "e03e6cb4d694fcb2b5ba",
+    "url": "/menu-builder/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "512d91fee8d090fbc9a2",
+    "url": "/menu-builder/static/css/main.f46dd96c.chunk.css"
+  },
+  {
+    "revision": "e03e6cb4d694fcb2b5ba",
+    "url": "/menu-builder/static/js/2.0fdb229a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/menu-builder/static/js/2.0fdb229a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "512d91fee8d090fbc9a2",
+    "url": "/menu-builder/static/js/main.666f83f9.chunk.js"
+  },
+  {
+    "revision": "aad4f68cf6757ffcca4b",
+    "url": "/menu-builder/static/js/runtime-main.19a57529.js"
+  }
+]);
