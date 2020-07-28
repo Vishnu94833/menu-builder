355bbf630c2be3af3d4957139d87a089b5ea30a3
K Vishnu <vishnu.a.k@capgemini.com>
2020-07-28 21:22:21 +0530

    logo

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,29 +1,23 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux';
-import { Dishes } from './dishes';
-import { Comments } from './comments';
-import { Promotions } from './promotions';
-import { Leaders } from './leaders';
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Dishes } from "./dishes";
+import { Comments } from "./comments";
+import { Promotions } from "./promotions";
+import { Leaders } from "./leaders";
 
-import thunk from 'redux-thunk';
-import { Feedback } from './feedback';
-import logger from 'redux-logger';
-import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import { Feedback } from "./feedback";
 
 export const ConfigureStore = () => {
-    const store = createStore(
-        combineReducers({
-            dishes: Dishes,
-            comments: Comments,
-            promotions: Promotions,
-            leaders: Leaders,
-            feedBack:Feedback
-        }),
-        composeWithDevTools(
-            applyMiddleware(thunk,logger)
-            // other store enhancers if any
-          )
-        
-    );
+  const store = createStore(
+    combineReducers({
+      dishes: Dishes,
+      comments: Comments,
+      promotions: Promotions,
+      leaders: Leaders,
+      feedBack: Feedback,
+    }),
+    applyMiddleware(thunk)
+  );
 
-    return store;
-}
+  return store;
+};
