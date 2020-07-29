cdb5930a1796106283808ad83e0b8a8310d06705
K Vishnu <vishnu.a.k@capgemini.com>
2020-07-29 13:02:14 +0530

    about us

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -48,12 +48,7 @@ function About(props) {
         <div className="col-12 col-md-6">
           <h2>Our History</h2>
           <p>
-            Started in 2010, Ristorante con Fusion quickly established itself as
-            a culinary icon par excellence in Hong Kong. With its unique brand
-            of world fusion cuisine that can be found nowhere else, it enjoys
-            patronage from the A-list clientele in Hong Kong. Featuring four of
-            the best three-star Michelin chefs in the world, you never know what
-            will arrive on your plate the next time you visit us.
+          Lorem ipsum dolor sit amet consectetur adipiscing elit, urna consequat felis vehicula class ultricies mollis dictumst, aenean non a in donec nulla. Phasellus ante pellentesque erat cum risus consequat imperdiet aliquam, integer placerat et turpis mi eros nec lobortis taciti, vehicula nisl litora tellus ligula porttitor metus.
           </p>
           <p>
             The restaurant traces its humble beginnings to{" "}
