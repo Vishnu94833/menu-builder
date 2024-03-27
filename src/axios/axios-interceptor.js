93c8e959487ea9d9fea638c822608f976fca044a
Vishnu94833 <vishnu94833@gmail.com>
2024-03-27 17:27:30 +0530

    dummy

--- src/axios/axios-interceptor.js
+++ src/axios/axios-interceptor.js
@@ -1,12 +1,26 @@
 import axios from "axios";
 import { baseUrl } from "../shared/baseUrl";
 
+const bearer_token = axios.post(
+  'https://login.salesforce.com/services/oauth2/token',
+  'grant_type=password&client_id=3MVG9n_HvETGhr3AazYjvGsJ70HSZv9TqYY81dRsIJyTi7.zPcREMYOSlUCAaSWvjnX8GF3.98xdTuLEGzPSJ&client_secret=C747F19398D0897C2196E1CB77FA4DA6B9827372853036FCD1D0C46DC12FB0DE&username=vishnukuppan1796@gmail.com&password=1hk16CS003',
+  {
+    headers: {
+      'Access-Control-Allow-Origin' : '*',
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  }
+).then((res)=>{
+
+  debugger
+})
+
 const instance = axios.create({
   baseURL:
   baseUrl,
   headers: {
     'Access-Control-Allow-Origin' : '*',
-    'Authorization':"Bearer 00D2w000001NWR3!ARYAQNnnY3Mrh41wIWbxUnv9d2W4.wX39WdQ7knE4474U1qGyGLiGcyci3JK6sxD2pEtA0.66B5qbiREN0y2ov28uk8zfgwr",
+    'Authorization':"Bearer 00D2w000001NWR3!ARYAQPqUrxXnPUelWd8URFOskeXim7d8nog8ReOckjkpycfSlZ0BILoPin7A6tWsR99uRtTd7gLIlMlfJ_Z0O7Y0.plBXKGs",
     'Content-Type': 'application/json'
   },
   mode: 'no-cors'
